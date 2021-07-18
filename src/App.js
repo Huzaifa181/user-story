416aa497ccfb28297cf3a368dffdf922384616d7
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-07-18 20:09:14 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
 import Layout from './Layout'
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
-import { Provider } from 'react-redux';
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
